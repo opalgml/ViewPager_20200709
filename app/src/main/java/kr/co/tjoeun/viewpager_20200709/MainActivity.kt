85b62f34d1d76a3af352cb61071e22a5fb2ace42
김미희 <opalgml@naver.com>
2020-07-09 21:49:29 +0900

    뷰 페이저 마무리

--- app/src/main/java/kr/co/tjoeun/viewpager_20200709/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200709/MainActivity.kt
@@ -2,8 +2,14 @@ package kr.co.tjoeun.viewpager_20200709
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.viewpager_20200709.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+//    어댑터를 멤버변수로 생성
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +23,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        뷰페이저용 어댑터 변수를 초기화
+        mvpa = MainViewPagerAdapter(supportFragmentManager) //AppCompatActivity 에서 제공하는  supportFragmentManager 사용
+        mainViewPager.adapter = mvpa
 
     }
 }
