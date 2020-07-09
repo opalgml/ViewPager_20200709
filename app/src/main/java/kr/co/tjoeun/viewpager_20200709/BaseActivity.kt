49fd0a913ee8dc5a5acfcd680b4b7ae006f4b663
김미희 <opalgml@naver.com>
2020-07-09 21:15:34 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200709/BaseActivity.kt
@@ -0,0 +1,10 @@
+package kr.co.tjoeun.viewpager_20200709
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
